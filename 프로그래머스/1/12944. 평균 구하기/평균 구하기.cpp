61d12c94aa49f8e31827eb38f1e34e383fb9a3a8
NMEii <82252932+NMEii@users.noreply.github.com>
2025-12-22 17:53:49 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    
+    for (const auto& it : arr)
+    {
+        answer += it;
+    }
+    
+    return answer / arr.size();
+}
