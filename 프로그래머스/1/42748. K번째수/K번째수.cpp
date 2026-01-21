b520bb681979712bc87fef034da298d57ef9b2ce
NMEii <82252932+NMEii@users.noreply.github.com>
2026-01-21 09:20:09 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> temp;
+    for (vector<int> inner : commands)
+    {
+        for (int i = inner[0]-1; i <= inner[1]-1; i++)
+        {
+            temp.push_back(array[i]);
+        }
+        sort(temp.begin(), temp.end());
+        answer.push_back(temp[inner[2]-1]);
+        temp.clear();
+    }
+    return answer;
+}
