28eeab75a7fa1288c5b63bcbc5ec29557895d35f
NMEii <82252932+NMEii@users.noreply.github.com>
2026-02-04 10:03:53 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.29 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    string temp = "";
+    for (int i = 1; i < food.size(); i++)
+    {
+        for (int j = 1; j <= food[i] / 2; j++)
+        {
+            temp += to_string(i);
+        }
+    }
+
+    answer += temp;
+    answer += "0";
+
+    sort(temp.begin(), temp.end(), greater<int>());
+
+    answer += temp;
+
+
+    return answer;
+}
