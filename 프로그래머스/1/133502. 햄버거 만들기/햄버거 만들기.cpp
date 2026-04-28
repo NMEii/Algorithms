64639550adcb9f6c3c5b4a98f1045decd648d1fa
NMEii <82252932+NMEii@users.noreply.github.com>
2026-04-28 12:39:34 +0900

    [level 1] Title: 햄버거 만들기, Time: 12.88 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    
+    vector<int> st;
+
+    for (int x : ingredient) {
+
+        st.push_back(x);
+
+        if (st.size() >= 4) {
+            
+            int n = st.size();
+
+            if (st[n - 4] == 1 && 
+                st[n - 3] == 2 && 
+                st[n - 2] == 3 && 
+                st[n - 1] == 1) {
+
+                answer++;
+
+                st.pop_back();
+                st.pop_back();
+                st.pop_back();
+                st.pop_back();
+            }
+        }
+    }
+
+    return answer;
+}
